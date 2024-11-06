fcb1e3677a014a746a1ccb9bdcea12ccbd7d887b
paulocavalheiro <paulohc.avila@gmail.com>
2024-11-06 09:33:34 -0300

    fiex:vercel

--- @types/module.d.ts
+++ @types/module.d.ts
@@ -1,2 +1,3 @@
 declare module "bime_financeiro/pages/dashboards/listaFinanceiro";
 declare module "bime_vendas/pages/listaVendas";
+declare module "bime_vendas/pages/dashboards/listaVendas";
---
+++ @types/moduleDeclarations.d.ts
@@ -0,0 +1,1 @@
+declare module "bime_vendas/pages/dashboards/listaVendas";
